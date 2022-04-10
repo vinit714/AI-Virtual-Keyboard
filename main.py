509f5645a9af45049044d9675a976166adce8212
Siddharth Sawhney <78915681+clumsybats@users.noreply.github.com>
2022-04-10 17:03:36 +0530

    Update main.py
    Added button positions and text inputs

--- main.py
+++ main.py
@@ -29,3 +29,34 @@ def drawAll(img, buttonList):
                     cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
     return img
  
+#
+# def drawAll(img, buttonList):
+#     imgNew = np.zeros_like(img, np.uint8)
+#     for button in buttonList:
+#         x, y = button.pos
+#         cvzone.cornerRect(imgNew, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
+#                           20, rt=0)
+#         cv2.rectangle(imgNew, button.pos, (x + button.size[0], y + button.size[1]),
+#                       (255, 0, 255), cv2.FILLED)
+#         cv2.putText(imgNew, button.text, (x + 40, y + 60),
+#                     cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
+#
+#     out = img.copy()
+#     alpha = 0.5
+#     mask = imgNew.astype(bool)
+#     print(mask.shape)
+#     out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
+#     return out
+ 
+ 
+class Button():
+    def __init__(self, pos, text, size=[85, 85]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+ 
+ 
+buttonList = []
+for i in range(len(keys)):
+    for j, key in enumerate(keys[i]):
+        buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
